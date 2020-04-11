b7ce88abf130d37eed57dd1889be24485a183240
Mitesh Upadhyay <upadhyaymitesh91.com>
2020-04-11 16:56:28 +0530

    Changes for Unit Testing

---
+++ src/test/java/com/mitesh/security/RestSecurity/testUtils/TestConstants.java
@@ -0,0 +1,15 @@
+package com.mitesh.security.RestSecurity.testUtils;
+
+public class TestConstants {
+
+	public static final String API_TRACE_ID="Test-Trace-Id";
+	
+	public static final String TEST_PUBLISHER_NAME="TestPublisherName";
+	public static final String TEST_PUBLISHER_EMAIL="TestPublisherName@email.com";
+	public static final String TEST_PUBLISHER_PHONE="112-333-444";	
+	
+	
+	public static final String TEST_PUBLISHER_EMAIL_UPDATED="TestPublisherNameUpdated@email.com";
+	public static final String TEST_PUBLISHER_PHONE_UPDATED="777-333-444";	
+	
+}
