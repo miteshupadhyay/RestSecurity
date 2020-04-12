148b2b1772be0375892344200362dfbf0ec5737e
Mitesh Upadhyay <upadhyaymitesh91.com>
2020-04-12 23:01:49 +0530

    User API Changes

---
+++ src/main/java/com/mitesh/security/RestSecurity/user/UserRepository.java
@@ -0,0 +1,18 @@
+package com.mitesh.security.RestSecurity.user;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Integer> {
+
+    UserEntity findByUsername(String username);
+
+    List<UserEntity> findByFirstNameAndLastNameContaining(String firstName, String lastName);
+
+    List<UserEntity> findByFirstNameContaining(String firstName);
+
+    List<UserEntity> findByLastNameContaining(String lastName);
+}
